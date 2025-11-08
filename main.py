f8d67a6e998726270d9ec88d974501dc8ee8c1bf
Nico Heinola <nico.heinola@gmail.com>
2025-11-08 18:59:12 +0200

    Add Vosk speech-to-text helper and update .gitignore; enhance logging in main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ Home Assistant Voice Console
 Type text to be sent to Jaakko 1 voice assistant via conversation service.
 """
 
+import logging
 import os
 from dotenv import load_dotenv
 
@@ -12,6 +13,9 @@ from home_assistant_api.text_to_speech.text_to_speech import HATextToSpeechAPI
 # Load environment variables
 load_dotenv()
 
+# Setup logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]: %(message)s")
+
 # Configuration
 HA_URL = os.getenv("HA_URL", "")
 HA_TOKEN = os.getenv("HA_TOKEN", "")
@@ -19,7 +23,7 @@ TEXT_TO_SPEECH_VOICE_ENGINE_ID = os.getenv("TEXT_TO_SPEECH_VOICE_ENGINE_ID", "")
 
 
 def main():
-    print("Running Home Assistant Voice Console. Type your message and press Enter.")
+    logging.info("Starting Home Assistant")
 
     text_to_speech_api: HATextToSpeechAPI = HATextToSpeechAPI(HA_URL, HA_TOKEN)
     text_to_speech_api.speak(TEXT_TO_SPEECH_VOICE_ENGINE_ID, "Working Text-to-Speech integration.")
