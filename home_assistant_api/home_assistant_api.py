f0afe5bc9f3beeec0665d40d9cc61707ede10fe9
Nico Heinola <nico.heinola@gmail.com>
2025-11-08 19:14:38 +0200

    Implement Home Assistant API for TTS and Wiz Lights; refactor imports and remove deprecated TTS integration

--- home_assistant_api/home_assistant_api.py
+++ home_assistant_api/home_assistant_api.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class HomeAssistantAPI:
     """Home Assistant API integration base class"""
 
@@ -9,3 +12,23 @@ class HomeAssistantAPI:
     def _authorization_header(self) -> dict:
         """Return authorization header for requests."""
         return {"Authorization": f"Bearer {self._access_token}"}
+
+    def _call_service(
+        self, domain: str, service: str, payload: dict = {}, extra_headers: dict = {}
+    ) -> requests.Response:
+        """Call a Home Assistant service.
+
+        Args:
+            domain: Service domain (e.g., "light", "media_player").
+            service: Service name (e.g., "turn_on", "play_media").
+            payload: JSON payload to send with the request.
+            extra_headers: Additional headers to include in the request.
+        """
+
+        headers = {
+            **self._authorization_header,
+            "Content-Type": "application/json",
+            **extra_headers,
+        }
+
+        return requests.post(f"{self._ha_url}/api/services/{domain}/{service}", json=payload, headers=headers)
--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import logging
 import os
 from dotenv import load_dotenv
 
-from home_assistant_api.text_to_speech.text_to_speech import HATextToSpeechAPI
+from home_assistant_api.text_to_speech.text_to_speech_api import HATextToSpeechAPI
+from home_assistant_api.wiz.wiz_lights_api import HAWizLightsAPI
 
 # Load environment variables
 load_dotenv()
@@ -25,8 +26,16 @@ TEXT_TO_SPEECH_VOICE_ENGINE_ID = os.getenv("TEXT_TO_SPEECH_VOICE_ENGINE_ID", "")
 def main():
     logging.info("Starting Home Assistant")
 
-    text_to_speech_api: HATextToSpeechAPI = HATextToSpeechAPI(HA_URL, HA_TOKEN)
-    text_to_speech_api.speak(TEXT_TO_SPEECH_VOICE_ENGINE_ID, "Working Text-to-Speech integration.")
+    # text_to_speech_api: HATextToSpeechAPI = HATextToSpeechAPI(HA_URL, HA_TOKEN)
+    # text_to_speech_api.speak(TEXT_TO_SPEECH_VOICE_ENGINE_ID, "Working Text-to-Speech integration.")
+
+    wiz_lights_api: HAWizLightsAPI = HAWizLightsAPI(HA_URL, HA_TOKEN)
+    success = wiz_lights_api.turn_on("wiz_lamp_workstation_1")
+    logging.info(f"Wiz light turn off successful: {success}")
+
+    # Get light state
+    state = wiz_lights_api.get_light_state("wiz_lamp_workstation_1")
+    logging.info(f"Wiz light state: {state}")
 
 
 if __name__ == "__main__":
