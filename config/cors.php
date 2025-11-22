804085d3ea42585ee9f9d8acd7df508d5039584e
Nico Heinola <nico.heinola@gmail.com>
2025-11-22 21:28:48 +0200

    feat: add CORS configuration file for API access control

---
+++ config/cors.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure CORS settings for your application. CORS is used
+    | to allow requests from different origins to access your API.
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+];
